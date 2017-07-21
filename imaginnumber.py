9e85f1269524c9327ede3ff46a1738db7795ae33
AtlantianApurv <apurva.beis.14@acharya.ac.in>
2017-07-20 23:39:19 -0400

    Create imaginnumber.py

---
+++ imaginnumber.py
@@ -0,0 +1,9 @@
+print ('imagin any number')
+print("Multiply the number with 5")
+print("add 10 to the result");
+print("subtract 6 from it");
+print("enter the last digit(the unit digit) of the number")
+no=input('emter the last digit of the no')
+no=(no+6-10)/5
+print("the imagined number is:")
+print (no)
