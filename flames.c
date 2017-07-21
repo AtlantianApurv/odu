2321edaab24182dfc221c1d465fa703d698c7a16
AtlantianApurv <apurva.beis.14@acharya.ac.in>
2017-07-21 11:15:48 +0530

    Update flames.c

--- flames.c
+++ flames.c
@@ -28,7 +28,7 @@ void main()
     for(int j=0;j<l2;j++)
       if(s1[i]==s2[j])
         {
-            s1[i]=s2[j]='?';
+            s1[i]=s2[j]='?'; //removing matched charcters
             match=match+1;
             break;
         }
@@ -36,23 +36,25 @@ void main()
   printf("string s1 after cancellation %s\n",s1);
   printf("string s2 after cancellation %s\n",s2);
   
+  /*avoiding any digit/ numbers in the Name1*/
   for(int i=0;i<l1;i++)
     if(isalpha(s1[i])==0)
       unms1++;
   
+  /*avoiding any digit/ numbers in the Name1*/
   for(int i=0;i<l2;i++)
     if(isalpha(s2[i])==0)
       unms2++;
     
     unm=unms1+unms2;  
     
-  strcat(s1,s2);
+  strcat(s1,s2); //conacatinate both the string for as to get as to get the count easily
   l3=strlen(s1);
   unm=l3-unm;
   
   printf("total unmatched characters %d\n",unm);
   
-  // Simple Approch
+  // Simple Approch (with pre calculated values)
   if(unm == 2 ||unm== 4 || unm==7 ||unm==9 )
    printf("%s and %s FLAMES as ENEMY \n",s1,s2);
   else if( unm == 3 ||unm==5 || unm==14 ||unm==16 || unm==18 )
@@ -70,14 +72,15 @@ void main()
   //Circular linked list Approch
   
   flames *head,*ne2,*ne3,*ne4,*ne5,*ne6,*temp,*present;
-  
+  //creating 6 nodes
   head=(struct flames*)malloc(sizeof(struct flames));
   ne2=(struct flames*)malloc(sizeof(struct flames));
   ne3=(struct flames*)malloc(sizeof(struct flames));
   ne4=(struct flames*)malloc(sizeof(struct flames));
   ne5=(struct flames*)malloc(sizeof(struct flames));
   ne6=(struct flames*)malloc(sizeof(struct flames));
-
+  //assigning each nodes with each charater of FLAMES and each pointer or the link pointd to the next character except the last
+  //character that actually points back to head i.e F
   head->c='f'; head->next=ne2;
   ne2->c='l'; ne2->next=ne3;
   ne3->c='a'; ne3->next=ne4;
@@ -87,6 +90,8 @@ void main()
   
   temp=head;
   int i=0;
+  
+  /*dispalying content of each node*/
   while(i<6)
   {
     printf(" %c",temp->c-32);
@@ -96,19 +101,21 @@ void main()
   printf("\n");
 
   temp=head;
-  while(m<5)
+  while(m<5) //outer loop till 5 count beacuse we need the last left character as it will be the output 
   { 
-      for(i=0;i<unm-2;i++)
+      for(i=0;i<unm-2;i++) //running the loop over FLAMES till the unmatched count 
     
-      temp=temp->next;
-      printf("\nDeleting %c\n",temp->next->c);
+      temp=temp->next;//first node will always points to the next (head will always points to its next node)
+      printf("\nDeleting %c\n",temp->next->c);  //printing deleted node
     
-      present=temp->next;
+      present=temp->next;//storing the node to another varible
       
-      temp->next=temp->next->next;
-      
+    //removing the link of node and assigning it ot another 
+        
+      temp->next=temp->next->next; 
       temp=temp->next;
-      free(present);
+      
+      free(present);//deleting the node 
       m++;
   }
   ch=temp->c;
